08cc6e1e0000de0ea64eb0763b92884331d2746d
debustos <debustos.dbp@gmail.com>
2024-03-25 11:52:29 +0100

    arreglo de verificacion de usuario

--- servidor/sql.c
+++ servidor/sql.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-int *verificarUsuario(char *user ,const char *database){
+int *verificarUsuario(char *user, const char *database) {
     sqlite3 *db;
     int *rest = (int *)malloc(sizeof(int));
     if (rest == NULL) {
@@ -23,32 +23,31 @@ int *verificarUsuario(char *user ,const char *database){
     }
     
     // Realizar operaciones con la base de datos...
-        const char *sql = "SELECT id FROM usuario WHERE nombre = ?;";
-         sqlite3_stmt *stmt;
-             rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-             rc = sqlite3_bind_text(stmt, 1, user, -1, SQLITE_STATIC);
+    const char *sql = "SELECT id FROM usuario WHERE nombre = ?;";
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    rc = sqlite3_bind_text(stmt, 1, user, -1, SQLITE_STATIC);
 
-        if (rc != SQLITE_OK) {
-            fprintf(stderr, "Error en la consulta: %s\n", sqlite3_errmsg(db));
-            sqlite3_close(db);
-            return rest;
-        }
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error en la consulta: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return rest;
+    }
 
-        // Ejecutar la consulta y procesar los resultados
-        int id;
-        while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-            id = sqlite3_column_int(stmt, 0);
-        }
-            printf("ID: %i\n", id);
-
-        
-        sqlite3_finalize(stmt);
+    // Ejecutar la consulta y procesar los resultados
+    int id = -1; // Inicializa id como un entero normal, no un puntero
+    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        id = sqlite3_column_int(stmt, 0);
+    }
+    printf("ID: %i\n", id);
+    
+    sqlite3_finalize(stmt);
     // Cerrar la base de datos cuando hayas terminado
     sqlite3_close(db);
-    *rest = id;
+    *rest = id; // Asigna el valor de id al puntero rest
     return rest;
-
 }
+
 void *verificarContrasenya(int *id, char *contrasenya){
     
 }
