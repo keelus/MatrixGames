45d26561f9a203920aacb389f2c067d2d21422e2
mikel.o.uria <mikel.o.uria@opendeusto.es>
2024-04-03 12:58:53 +0200

    Empezado a implementar el menu de estadisticas

--- servidor/sql.cpp
+++ servidor/sql.cpp
@@ -32,12 +32,24 @@ bool crearUsuario(const char *usuario, const char *contrasenya, int *idusuario){
 bool verUsuario(const char *usuario ){
 	sqlite3pp::database db("baseDeDatos.db");
 
-	sqlite3pp::query qry(db, "SELECT id FROM USUARIO WHERE nombre = :nombre ");
-	qry.bind(":nombre", usuario, sqlite3pp::nocopy);
-	int idUsuario = -1;
-	for (sqlite3pp::query::iterator i = qry.begin(); i != qry.end(); ++i) {
+	sqlite3pp::query qry(db, "SELECT id FROM USUARIO WHERE nombre = :nombre ");//Consulta
+	qry.bind(":nombre", usuario, sqlite3pp::nocopy);//Une la busqueda
+	int idUsuario = -1;//Torna el ID del usuario a -1
+	for (sqlite3pp::query::iterator i = qry.begin(); i != qry.end(); ++i) {//For desde el principio al final del qry
 		std::tie(idUsuario) = (*i).get_columns<int>(0);
 	}
 
 	return idUsuario >= 0;
+}
+bool verStats(const char *idUsuario){
+	sqlite3pp::database db("baseDeDatos.db");//Esto conecta con la bd
+	
+	sqlite3pp::query qryA(db, "SELECT puntuacion FROM USUARIO A INNER JOIN PARTIDAS_SINGLEPLAYER B ON A.id = B.idUsuario WHERE A.id = :id ");
+	qryA.bind(":id",idUsuario, sqlite3pp::nocopy);
+	
+	sqlite3pp::query qryB(db, "SELECT * FROM USUARIO A INNER JOIN PARTIDAS_MULTIPLAYER B ON A.id = B.idJugador WHERE A.id = :id ");
+	qryB.bind(":id",idUsuario, sqlite3pp::nocopy);
+
+	printf("");
+	return true;
 }
