5b15bae8b8f6499f7f41c569e75a44e79aa4d360
keelus <hugomoreda@hotmail.com>
2024-05-09 15:45:33 +0200

    Arreglar tablero equivocado en uso.

--- servidor/juegos/flota/partida.cpp
+++ servidor/juegos/flota/partida.cpp
@@ -17,21 +17,21 @@ bool flota::Partida::Iteracion(int socketId, MatrizLED *matrizLED) {
 	ColorLED bufferContenido[8][8];
 
 	if (Turno == TiposTurno::TURNO_JUGADOR) {
-		TableroJugador.AContenidoColor(bufferContenido, false, true);
+		TableroCPU.AContenidoColor(bufferContenido, false, true);
 		matrizLED->SetMatrizColor(bufferContenido);
 
-		TableroJugador.CoordenadaDeAtaque.X = 0;
-		TableroJugador.CoordenadaDeAtaque.Y = 0;
+		TableroCPU.CoordenadaDeAtaque.X = 0;
+		TableroCPU.CoordenadaDeAtaque.Y = 0;
 
 		bool ataqueRealizado = false;
-		bool errorAtaqueYaRealizao = false;
+		bool errorAtaqueYaRealizado = false;
 
 		while (!ataqueRealizado) {
 			std::string contenidoPrincipal = "Te toca atacar!\nPuedes ver el tablero de tu rival, y tus ataques en la matriz LED.";
 			contenidoPrincipal += "\n\nMueve el punto de ataque con W A S y D.\nUsa la tecla Enter para atacar!";
-			contenidoPrincipal += "\n\nCoordenada de ataque actual -> (Fila: " + std::to_string(TableroJugador.CoordenadaDeAtaque.Y) + ", Columna: " + std::to_string(TableroJugador.CoordenadaDeAtaque.X) + ")";
+			contenidoPrincipal += "\n\nCoordenada de ataque actual -> (Fila: " + std::to_string(TableroCPU.CoordenadaDeAtaque.Y) + ", Columna: " + std::to_string(TableroCPU.CoordenadaDeAtaque.X) + ")";
 
-			if (errorAtaqueYaRealizao)
+			if (errorAtaqueYaRealizado)
 				contenidoPrincipal += "\n\nYa has realizado ese ataque!";
 
 			paquetes::MandarPaquete(socketId, contenidoPrincipal, "\n\n[ Pulsa una tecla ] ", PULSACION, true);
@@ -44,31 +44,31 @@ bool flota::Partida::Iteracion(int socketId, MatrizLED *matrizLED) {
 			switch (paqueteDeCliente.GetContenido()[0]) {
 			case 'A':
 			case 'a':
-				if (TableroJugador.CoordenadaDeAtaque.X > 0)
-					TableroJugador.CoordenadaDeAtaque.X--;
+				if (TableroCPU.CoordenadaDeAtaque.X > 0)
+					TableroCPU.CoordenadaDeAtaque.X--;
 				break;
 			case 'D':
 			case 'd':
-				if (TableroJugador.CoordenadaDeAtaque.X < 7)
-					TableroJugador.CoordenadaDeAtaque.X++;
+				if (TableroCPU.CoordenadaDeAtaque.X < 7)
+					TableroCPU.CoordenadaDeAtaque.X++;
 				break;
 			case 'W':
 			case 'w':
-				if (TableroJugador.CoordenadaDeAtaque.Y > 0)
-					TableroJugador.CoordenadaDeAtaque.Y--;
+				if (TableroCPU.CoordenadaDeAtaque.Y > 0)
+					TableroCPU.CoordenadaDeAtaque.Y--;
 				break;
 			case 'S':
 			case 's':
-				if (TableroJugador.CoordenadaDeAtaque.Y < 7)
-					TableroJugador.CoordenadaDeAtaque.Y++;
+				if (TableroCPU.CoordenadaDeAtaque.Y < 7)
+					TableroCPU.CoordenadaDeAtaque.Y++;
 				break;
 			case ' ':
-				if (TableroCPU.AtaqueYaRecibido(TableroJugador.CoordenadaDeAtaque)) {
-					errorAtaqueYaRealizao = true;
+				if (TableroCPU.AtaqueYaRecibido(TableroCPU.CoordenadaDeAtaque)) {
+					errorAtaqueYaRealizado = true;
 					std::cout << "Ataque ya realizado!\n";
 				} else {
 					ataqueRealizado = true;
-					Ataque resultadoAtaque = TableroCPU.RecibirAtaque(TableroJugador.CoordenadaDeAtaque);
+					Ataque resultadoAtaque = TableroCPU.RecibirAtaque(TableroCPU.CoordenadaDeAtaque);
 					haAcertado = resultadoAtaque.EsHit;
 
 					std::string tableroStr = TableroCPU.AString(true, false);
